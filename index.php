6c74985de14f73ef053566130790acdd4e2b5799
Blitzi34 <73278109+Blitzi34@users.noreply.github.com>
2024-06-17 15:37:24 +0200

    Muster hinzugefügt

--- index.php
+++ index.php
@@ -4,22 +4,41 @@ if (!isset( $_SESSION )) {
 }
 
 
-echo '
-<html>
+$template['name'] 	       	= (!empty($_GET['template'])) ? ($_GET['template']) : ('');
+$template['template_file']  = "{$_SERVER['DOCUMENT_ROOT']}/templates/{$template['name']}.php";
+$template['location']       = "/templates/{$template['name']}.php";
 
-    <head>
-        <title>Login-Projekt</title>
-        <link rel="stylesheet" href="" type="text/css">
-        <style type="text/css">
-        </style>
-    </head>
 
-    <body>
-        <div>
-            TEST 2
-        </div> 
+if (!file_exists($template['template_file'])) {
+    echo "{$template['name']} -> Das Template konnte nicht gefunden werden";
+    exit;
+} 
 
-    </body>
-</html>';
+include_once($template['template_file']);
+$template['content'] 	= call_user_func($template['name']);
+
+$html_output =
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login-Projekt</title>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+    <div id="template-page-content" class="m-0 pb-0 p-1">			
+        '.$template['content'].'
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+    </div>
+</body>';
+
+echo $html_output;
 
 ?>
---
+++ templates/sample.php
@@ -0,0 +1,18 @@
+<?php
+
+
+include_once("{$_SERVER['DOCUMENT_ROOT']}/modules/sample.php");
+
+    function sample() {
+
+        $html_output =
+        '<div>
+            Test Template
+        </div>
+
+        <script src="/js/sample.js?version='.time().'" type="text/javascript"></script>';
+    
+        return $html_output;
+    }
+
+?>
