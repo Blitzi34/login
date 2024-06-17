1fa23a37b8fb6d0a80cc20c9a4985d5a0c216bba
Blitzi34 <73278109+Blitzi34@users.noreply.github.com>
2024-06-17 11:16:21 +0200

    start mit Index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset( $_SESSION )) {
+	session_start();
+}
+
+
+echo '
+<html>
+
+    <head>
+        <title>Login-Projekt</title>
+        <link rel="stylesheet" href="" type="text/css">
+        <style type="text/css">
+        </style>
+    </head>
+
+    <body>
+        <div>
+            TEST
+        </div>
+
+    </body>
+</html>';
+
+?>
